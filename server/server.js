dac2eab434a7a67a03cc093f6df9d8b3b1aaf9ac
SecretMNC <pettkevinj@gmail.com>
2017-12-18 12:02:51 -0700

    connect database to the server

--- server/server.js
+++ server/server.js
@@ -8,6 +8,10 @@ const express = require("express")
 app.use(bodyParser.json());
 app.use(express.static(`${__dirname}/../build`));
 
+massive(process.env.CONNECTION_STRING).then((db) => {
+    console.log('db is connected');
+    app.set('db', db);
+});
 
 // create a bio
 app.post('/api/create/bios', (req, res) => {
@@ -50,3 +54,5 @@ app.delete('/api/delete/bios/:yourname', (req, res) => {
             res.status(200).send(bio)
         }).catch(err => res.status(500).send(err))
 });
+
+app.listen(PORT, () => (console.log(`Listening on port ${PORT}`)));
